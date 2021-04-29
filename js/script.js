7e261c418c9b55b177e09a13c65bc9616f074699
enrico bianco <biancoenrico71@gmail.com>
2021-04-29 10:04:45 +0200

    fatta select

--- js/script.js
+++ js/script.js
@@ -2,23 +2,40 @@ var app = new Vue({
     el: '#root',
     data: {
         playlist:[],
-        sortedPlaylist:[]
+        options: ['All'],
+        select: 'All'
     },
     methods: {
-
+        
+        
     },
+
     mounted(){
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((response) => {
-                
+
                 const albums = response.data;
                 this.playlist = albums.response;
 
-                sortedPlaylist = this.playlist.sort(function(a,b){
+                this.playlist = this.playlist.sort(function(a,b){
                     return a.year - b.year;
                 })
+
+                this.playlist.forEach((element) =>{
+
+                    if(!this.options.includes(element.genre) ){
+                        this.options.push(element.genre);
+                    }
+                    console.log(this.options);
+                });
+
+                  
+                
                 
             });
-    },
+            
+
+        },
+        
 })
